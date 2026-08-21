66d2d6c190c6876e06f8e34e7c0cd8656eb920ce
Dela <meet.delaa@gmail.com>
2026-08-21 21:44:00 +0000

    feat(bundle): wrap the engine in a Symfony bundle
    - Add the bundle class, DI extension and configuration
    - Wire the store and manager as services through
      Resources/config/services.yaml

---
+++ src/DependencyInjection/MagicLinkExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ossm\MagicLinkBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class MagicLinkExtension extends Extension
+{
+    /**
+     * @param array<mixed> $configs
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+
+        $tokenTtl = $config['token_ttl'];
+        if (! is_int($tokenTtl)) {
+            // The Configuration tree guarantees an integer; this guards the
+            // type PHPStan cannot see through processConfiguration().
+            throw new \LogicException('magic_link.token_ttl must be an integer.');
+        }
+        $container->setParameter('ossm_magic_link.token_ttl', $tokenTtl);
+    }
+}
